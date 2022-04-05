98068863b841b8bf7cc88522b2ae6a60bb0bde37
ryotaro-tenya0727 <ryotaro123110@gmail.com>
2022-04-05 10:03:27 +0900

    Fix: Router修正

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import Helmet from 'react-helmet';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
-import logo from './logo192.svg';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import './App.css';
 import { Home, About } from './containers';
 
@@ -11,26 +10,30 @@ class App extends Component {
       <div className='App'>
         <Helmet title='Default title' />
         <div className='App-header'>
-          <img src={logo} className='App-logo' alt='logo' />
           <h2>Welcome to React</h2>
         </div>
-        <p className='App-intro'>
-          <Router>
-            <div>
-              <ul>
-                <li>
-                  <Link to='/'>Home</Link>
-                </li>
-                <li>
-                  <Link to='/about'>About</Link>
-                </li>
-              </ul>
-              <hr />
-              <Route exact path='/' component={Home} />
-              <Route path='/about' component={About} />
-            </div>
-          </Router>
-        </p>
+
+        <Router>
+          <div>
+            <ul>
+              <li>
+                <Link to='/'>Home</Link>
+              </li>
+              <li>
+                <Link to='/about'>About</Link>
+              </li>
+            </ul>
+            <hr />
+            <Routes>
+              <Route path='/'>
+                <Route index element={<Home />} />
+                <Route path='about'>
+                  <Route index element={<About />} />
+                </Route>
+              </Route>
+            </Routes>
+          </div>
+        </Router>
       </div>
     );
   }
