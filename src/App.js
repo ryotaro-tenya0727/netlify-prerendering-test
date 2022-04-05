f7c69bee2cc38330ec16b4005e23cf29cc76843a
ryotaro-tenya0727 <ryotaro123110@gmail.com>
2022-04-05 09:50:02 +0900

    Fix: App.jsにルーティング追加

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import Helmet from 'react-helmet';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import logo from './logo192.svg';
 import './App.css';
+import { Home, About } from './containers';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+class App extends Component {
+  render() {
+    return (
+      <div className='App'>
+        <Helmet title='Default title' />
+        <div className='App-header'>
+          <img src={logo} className='App-logo' alt='logo' />
+          <h2>Welcome to React</h2>
+        </div>
+        <p className='App-intro'>
+          <Router>
+            <div>
+              <ul>
+                <li>
+                  <Link to='/'>Home</Link>
+                </li>
+                <li>
+                  <Link to='/about'>About</Link>
+                </li>
+              </ul>
+              <hr />
+              <Route exact path='/' component={Home} />
+              <Route path='/about' component={About} />
+            </div>
+          </Router>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      </div>
+    );
+  }
 }
 
 export default App;
